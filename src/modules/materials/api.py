d498d8e2b1193aaa329484af63ffd0af84bf612b
egork <onetells@ya.ru>
2026-05-18 22:15:23 +0500

    New Version

--- src/modules/materials/api.py
+++ src/modules/materials/api.py
@@ -4,7 +4,7 @@ from modules.materials.catalog.api import router as materials_catalog_router
 from modules.materials.categories.api import router as materials_categories_router
 from modules.materials.material.api import router as material_router
 
-router = APIRouter(prefix="/materials", tags=["Материалы"])
+router = APIRouter(prefix="/materials", tags=["Управление материалами"])
 router.include_router(materials_catalog_router)
 router.include_router(materials_categories_router)
 router.include_router(material_router)
--- src/modules/products/api.py
+++ src/modules/products/api.py
@@ -4,7 +4,7 @@ from modules.products.catalog.api import router as products_catalog_router
 from modules.products.categories.api import router as products_categories_router
 from modules.products.product.api import router as product_router
 
-router = APIRouter(prefix="/products", tags=["Продукты"])
+router = APIRouter(prefix="/products", tags=["Управление продуктами"])
 router.include_router(products_catalog_router)
 router.include_router(products_categories_router)
 router.include_router(product_router)
